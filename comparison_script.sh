522a2c262cda6340e2447fe47dd7d3f6c412a338
Dan Maljovec <maljovec@cs.utah.edu>
2018-06-12 17:14:09 -0600

    Cleaning "run everything" bash script up.

--- comparison_script.sh
+++ comparison_script.sh
@@ -4,46 +4,69 @@ build_dir=$(pwd)/data/misc
 input_dir=$(pwd)/data/input
 output_dir=$(pwd)/data/output
 
-rm -rf ngl-base ngl-omp nglpy build-ngl-base.txt build-ngl-omp.txt build-ngl-numba.txt build-ngl-cuda.txt
+D=3
+N=10
+K=10
 
-echo "===== Cloning and building ngl-base ====="
+build_file=$build_dir/build-ngl-
+
+rm -rf $build_dir/build-ngl-base.txt $build_dir/build-ngl-omp.txt $build_dir/build-ngl-numba.txt $build_dir/build-ngl-cuda.txt
+
+# TODO: Add check for "hard reset" which will perform this nuke, otherwise use
+# the existing directories to pull and build.
+# if [ 0 ]
+# then
+#   rm -rf ngl-base ngl-omp ngl-numba 
+# fi
+
+echo "===== Updating and building ngl-base ====="
 start=$(date +%s)
-echo "===== Cloning =====" > $build_dir/build-ngl-base.txt 2>&1
-git clone git@bitbucket.org:dmaljovec/ngl-beta.git ngl-base >> $build_dir/build-ngl-base.txt 2>&1
-mkdir ngl-base/build >> $build_dir/build-ngl-base.txt 2>&1
-pushd ngl-base/build >> $build_dir/build-ngl-base.txt 2>&1
-echo "===== Building =====" >> $build_dir/build-ngl-base.txt 2>&1
-cmake .. >> $build_dir/build-ngl-base.txt 2>&1
-make >> $build_dir/build-ngl-base.txt 2>&1
+if [ ! -d ngl-base ]
+then
+    echo "===== Cloning =====" &> $build_dir/build-ngl-base.txt
+    git clone git@bitbucket.org:dmaljovec/ngl-beta.git ngl-base &>> $build_dir/build-ngl-base.txt
+fi
+echo "===== Updating =====" &>> $build_dir/build-ngl-base.txt
+pushd ngl-base &>> $build_dir/build-ngl-base.txt
+git pull &>> $build_dir/build-ngl-base.txt
+echo "===== Building =====" &>> $build_dir/build-ngl-base.txt
+cmake . &>> $build_dir/build-ngl-base.txt
+make &>> $build_dir/build-ngl-base.txt
 popd > /dev/null
 end=$(date +%s)
 let "diff=$end-$start"
 echo "$diff seconds"
 
-echo "===== Cloning and building ngl-omp ====="
+echo "===== Updating and building ngl-omp ====="
 start=$(date +%s)
-echo "===== Cloning =====" > $build_dir/build-ngl-omp.txt 2>&1
-git clone git@bitbucket.org:dmaljovec/ngl-beta.git ngl-omp >> $build_dir/build-ngl-omp.txt 2>&1
-mkdir ngl-omp/build >> $build_dir/build-ngl-omp.txt 2>&1
-pushd ngl-omp >> $build_dir/build-ngl-omp.txt 2>&1
-git checkout omp >> $build_dir/build-ngl-omp.txt 2>&1
-git pull >> $build_dir/build-ngl-omp.txt 2>&1
-echo "===== Building =====" >> $build_dir/build-ngl-omp.txt 2>&1
-cd build >> $build_dir/build-ngl-omp.txt 2>&1
-cmake .. >> $build_dir/build-ngl-omp.txt 2>&1
-make >> $build_dir/build-ngl-omp.txt 2>&1
+if [ ! -d ngl-omp ]
+then
+    echo "===== Cloning =====" &> $build_dir/build-ngl-omp.txt
+    git clone git@bitbucket.org:dmaljovec/ngl-beta.git ngl-omp &>> $build_dir/build-ngl-omp.txt
+fi
+echo "===== Updating =====" &>> $build_dir/build-ngl-omp.txt
+pushd ngl-omp &>> $build_dir/build-ngl-omp.txt
+git checkout omp &>> $build_dir/build-ngl-omp.txt
+git pull &>> $build_dir/build-ngl-omp.txt
+echo "===== Building =====" &>> $build_dir/build-ngl-omp.txt
+cmake . &>> $build_dir/build-ngl-omp.txt
+make &>> $build_dir/build-ngl-omp.txt
 popd > /dev/null
 end=$(date +%s)
 let "diff=$end-$start"
 echo "$diff seconds"
 
-echo "===== Cloning and building ngl-numba ====="
+echo "===== Updating and building ngl-numba ====="
 start=$(date +%s)
-echo "===== Cloning =====" > $build_dir/build-ngl-numba.txt 2>&1
-git clone git@github.com:maljovec/nglpy.git nglpy >> $build_dir/build-ngl-numba.txt 2>&1
-pushd nglpy >> $build_dir/build-ngl-numba.txt 2>&1
-git checkout pure_python >> $build_dir/build-ngl-numba.txt 2>&1
-git pull >> $build_dir/build-ngl-numba.txt 2>&1
+if [ ! -d ngl-numba ]
+then
+    echo "===== Cloning =====" &> $build_dir/build-ngl-numba.txt
+    git clone git@github.com:maljovec/nglpy.git ngl-numba &>> $build_dir/build-ngl-numba.txt
+fi
+echo "===== Updating =====" &>> $build_dir/build-ngl-numba.txt
+pushd ngl-numba &>> $build_dir/build-ngl-numba.txt
+git checkout pure_python &>> $build_dir/build-ngl-numba.txt
+git pull &>> $build_dir/build-ngl-numba.txt
 popd > /dev/null
 end=$(date +%s)
 let "diff=$end-$start"
@@ -51,39 +74,38 @@ echo "$diff seconds"
 
 echo "===== Building ngl-gpu ====="
 start=$(date +%s)
-echo "===== Building =====" > $build_dir/build-ngl-cuda.txt 2>&1
-pushd ngl-gpu >> $build_dir/build-ngl-cuda.txt 2>&1
-nvcc prune.cu -lann -o prune_graph >> $build_dir/build-ngl-cuda.txt 2>&1
+echo "===== Building =====" > $build_dir/build-ngl-cuda.txt
+pushd ngl-gpu &>> $build_dir/build-ngl-cuda.txt
+nvcc prune.cu -lann -o prune_graph &>> $build_dir/build-ngl-cuda.txt
 popd > /dev/null
 end=$(date +%s)
 let "diff=$end-$start"
 echo "$diff seconds"
 
-pushd ngl-base/build > /dev/null
+pushd ngl-base > /dev/null
 echo "-----ngl-base------------------------------------------------------------"
-time binsrc/getNeighborGraph -d 2 -b 1 -k 100 -i $input_dir/data_2_1000000_0.csv > $output_dir/edges_2D_base.txt
+time binsrc/getNeighborGraph -d $D -b 1 -k $K -i $input_dir/data_${D}_${N}_0.csv > $output_dir/edges_${D}D_base.txt
 echo "-------------------------------------------------------------------------"
 popd > /dev/null
 
-pushd ngl-omp/build > /dev/null
+pushd ngl-omp > /dev/null
 echo "-----ngl-omp-------------------------------------------------------------"
-time binsrc/getNeighborGraph -d 2 -b 1 -k 100 -i $input_dir/data_2_1000000_0.csv > $output_dir/edges_2D_omp.txt
+time binsrc/getNeighborGraph -d $D -b 1 -k $K -i $input_dir/data_${D}_${N}_0.csv > $output_dir/edges_${D}D_omp.txt
 echo "-------------------------------------------------------------------------"
 popd > /dev/null
 
-pushd nglpy > /dev/null
+pushd ngl-numba > /dev/null
 echo "-----ngl-numba-----------------------------------------------------------"
-time python test_numba.py -i $input_dir/data_2_1000000_0.csv > $output_dir/edges_2D_numba.txt
+time python test_numba.py -i $input_dir/data_${D}_${N}_0.csv -s 999 > $output_dir/edges_${D}D_numba.txt
 echo "-------------------------------------------------------------------------"
 popd > /dev/null
 
 pushd ngl-gpu > /dev/null
-nvcc prune.cu -lann -o prune_graph
 echo "-----ngl-cuda-discrete---------------------------------------------------"
-time ./prune_graph -i $input_dir/data_2_1000000_0.csv -d 2 -n 1000000 -k 100 -b 1 -p 2 -s 9999 > $output_dir/edges_2D_gpu_discrete.txt
+time ./prune_graph -i $input_dir/data_${D}_${N}_0.csv -d $D -n $N -k $K -b 1 -p 2 -s 9999 > $output_dir/edges_${D}D_gpu_discrete.txt
 echo "-----ngl-cuda------------------------------------------------------------"
-time ./prune_graph -i $input_dir/data_2_1000000_0.csv -d 2 -n 1000000 -k 100 -b 1 -p 2 -s -1 > $output_dir/edges_2D_gpu.txt
+time ./prune_graph -i $input_dir/data_${D}_${N}_0.csv -d $D -n $N -k $K -b 1 -p 2 -s -1 > $output_dir/edges_${D}D_gpu.txt
 echo "-------------------------------------------------------------------------"
 popd > /dev/null
 
-python compare_edge_sets.py 2
+python compare_edge_sets.py $D
