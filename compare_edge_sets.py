3973def30b085881d85ff7495d912e55e3490dd2
Dan Maljovec <maljovec@cs.utah.edu>
2018-06-14 15:50:14 -0600

    Adding more formatting to the test bash script and automating more of the python comparison script to not care if file does not exist.

--- compare_edge_sets.py
+++ compare_edge_sets.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse 
+import os.path
 
 parser = argparse.ArgumentParser(description='Build an lp-beta skeleton using numba.')
 parser.add_argument('dimensionality', type=int,
@@ -8,26 +9,40 @@ parser.add_argument('dimensionality', type=int,
 args = parser.parse_args()
 D = args.dimensionality
 
-algorithms = ['base','omp','numba','gpu', 'gpu_discrete']
+algorithms = ['base',
+              'omp',
+              'numba',
+              'gpu',
+              'gpu_discrete']
 
 edges = {}
 eset = {}
+toRemove = []
 for alg in algorithms:
-    edges[alg] = np.loadtxt('data/output/edges_{}D_{}.txt'.format(D, alg), dtype=int)
-    eset[alg] = set()
-    
-    for edge in edges[alg]:
-        if edge[1] < edge[0]:
-            lo = edge[1]
-            hi = edge[0]
-        else:
-            lo = edge[0]
-            hi = edge[1]
+    fname = 'data/output/edges_{}D_{}.txt'.format(D, alg)
+    if os.path.isfile(fname):
+        edges[alg] = np.loadtxt(fname, dtype=int)
+        eset[alg] = set()
         
-        if lo != hi:
-            eset[alg].add((lo,hi))
-    
-    print('{:>12}: {}'.format(alg, len(eset[alg])))
+        for edge in edges[alg]:
+            if edge[1] < edge[0]:
+                lo = edge[1]
+                hi = edge[0]
+            else:
+                lo = edge[0]
+                hi = edge[1]
+            
+            if lo != hi:
+                eset[alg].add((lo,hi))
+        
+        print('{:>12}: {}'.format(alg, len(eset[alg])))
+    else:
+        toRemove.append(alg)
+
+for alg in toRemove:
+    algorithms.remove(alg)
+
+print('~ '*40)
 
 eset_base = eset['base']
 for alg in algorithms[1:]:
@@ -38,8 +53,3 @@ for alg in algorithms[1:]:
         print('Difference base vs {0}:\n         base only {1}\n {0:>12} only {2}'.format(alg, v1, v2))
     else: 
         print('No Difference base vs {0}'.format(alg))
-
-# print('='*80)
-# print(eset['numba'].difference(eset_base))
-# print('='*80)
-# print(eset_base.difference(eset['numba']))
--- comparison_script.sh
+++ comparison_script.sh
@@ -4,21 +4,25 @@ build_dir=$(pwd)/data/misc
 input_dir=$(pwd)/data/input
 output_dir=$(pwd)/data/output
 
-D=3
-N=10
-K=10
+D=2
+N=1000000
+K=100
+
+echo "D=$D N=$N K=$K"
 
 build_file=$build_dir/build-ngl-
 
 rm -rf $build_dir/build-ngl-base.txt $build_dir/build-ngl-omp.txt $build_dir/build-ngl-numba.txt $build_dir/build-ngl-cuda.txt
+rm -rf $output_dir/edges_${D}D_base.txt $output_dir/edges_${D}D_omp.txt $output_dir/edges_${D}D_numba.txt $output_dir/edges_${D}D_gpu.txt $output_dir/edges_${D}D_gpu_discrete.txt
 
+################################################################################
 # TODO: Add check for "hard reset" which will perform this nuke, otherwise use
 # the existing directories to pull and build.
 # if [ 0 ]
 # then
 #   rm -rf ngl-base ngl-omp ngl-numba 
 # fi
-
+################################################################################
 echo "===== Updating and building ngl-base ====="
 start=$(date +%s)
 if [ ! -d ngl-base ]
@@ -56,21 +60,21 @@ end=$(date +%s)
 let "diff=$end-$start"
 echo "$diff seconds"
 
-echo "===== Updating and building ngl-numba ====="
-start=$(date +%s)
-if [ ! -d ngl-numba ]
-then
-    echo "===== Cloning =====" &> $build_dir/build-ngl-numba.txt
-    git clone git@github.com:maljovec/nglpy.git ngl-numba &>> $build_dir/build-ngl-numba.txt
-fi
-echo "===== Updating =====" &>> $build_dir/build-ngl-numba.txt
-pushd ngl-numba &>> $build_dir/build-ngl-numba.txt
-git checkout pure_python &>> $build_dir/build-ngl-numba.txt
-git pull &>> $build_dir/build-ngl-numba.txt
-popd > /dev/null
-end=$(date +%s)
-let "diff=$end-$start"
-echo "$diff seconds"
+# echo "===== Updating and building ngl-numba ====="
+# start=$(date +%s)
+# if [ ! -d ngl-numba ]
+# then
+#     echo "===== Cloning =====" &> $build_dir/build-ngl-numba.txt
+#     git clone git@github.com:maljovec/nglpy.git ngl-numba &>> $build_dir/build-ngl-numba.txt
+# fi
+# echo "===== Updating =====" &>> $build_dir/build-ngl-numba.txt
+# pushd ngl-numba &>> $build_dir/build-ngl-numba.txt
+# git checkout pure_python &>> $build_dir/build-ngl-numba.txt
+# git pull &>> $build_dir/build-ngl-numba.txt
+# popd > /dev/null
+# end=$(date +%s)
+# let "diff=$end-$start"
+# echo "$diff seconds"
 
 echo "===== Building ngl-gpu ====="
 start=$(date +%s)
@@ -94,15 +98,17 @@ time binsrc/getNeighborGraph -d $D -b 1 -k $K -i $input_dir/data_${D}_${N}_0.csv
 echo "-------------------------------------------------------------------------"
 popd > /dev/null
 
-pushd ngl-numba > /dev/null
-echo "-----ngl-numba-----------------------------------------------------------"
-time python test_numba.py -i $input_dir/data_${D}_${N}_0.csv -s 999 > $output_dir/edges_${D}D_numba.txt
-echo "-------------------------------------------------------------------------"
-popd > /dev/null
+# pushd ngl-numba > /dev/null
+# echo "-----ngl-numba-----------------------------------------------------------"
+# time python test_numba.py -i $input_dir/data_${D}_${N}_0.csv -s 999 > $output_dir/edges_${D}D_numba.txt
+# echo "-------------------------------------------------------------------------"
+# popd > /dev/null
 
 pushd ngl-gpu > /dev/null
 echo "-----ngl-cuda-discrete---------------------------------------------------"
 time ./prune_graph -i $input_dir/data_${D}_${N}_0.csv -d $D -n $N -k $K -b 1 -p 2 -s 9999 > $output_dir/edges_${D}D_gpu_discrete.txt
+echo "-------------------------------------------------------------------------"
+
 echo "-----ngl-cuda------------------------------------------------------------"
 time ./prune_graph -i $input_dir/data_${D}_${N}_0.csv -d $D -n $N -k $K -b 1 -p 2 -s -1 > $output_dir/edges_${D}D_gpu.txt
 echo "-------------------------------------------------------------------------"
