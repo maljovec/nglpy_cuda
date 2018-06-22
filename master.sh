d7e37d206f5a0e783856b30218a1af7ef94552d3
Dan Maljovec <maljovec@cs.utah.edu>
2018-06-22 00:03:28 -0600

    Fix-up in master.sh

--- master.sh
+++ master.sh
@@ -6,7 +6,7 @@ time ./comparison_script.sh -c 1000000 -d 7 -k 300 &> ../7D_results.txt
 
 time ../knn-generator/binsrc/getNeighborGraph -d 8 -b 1 -k 300 -i ../ngl-gpu/data/input/data_8_1000000_0.csv 2> data/misc/knn_8D.txt
 
-time /comparison_script.sh -c 1000000 -d 8 -k 300 &> ../8D_results.txt
+time ./comparison_script.sh -c 1000000 -d 8 -k 300 &> ../8D_results.txt
 
 time ../knn-generator/binsrc/getNeighborGraph -d 9 -b 1 -k 300 -i ../ngl-gpu/data/input/data_9_1000000_0.csv 2> data/misc/knn_9D.txt
 
