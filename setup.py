33401b3b95fdce4a548607a5eaaf7a06e8ca994d
Dan Maljovec <maljovec@cs.utah.edu>
2018-09-19 04:45:59 -0600

    Implementing more testing.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy>=1.15']
+requirements = ['numpy']
 
 ###############################################################################
 # Code from https://github.com/rmcgibbo/npcuda-example to build a custom
