2ec2d547748c7a7563e59753e234e0c03d00959b
Dan Maljovec <maljovec@cs.utah.edu>
2018-09-19 03:11:16 -0600

    Adding utils file, updating __init__, and changing Graph to be an iterable.

--- nglpy_cuda/__init__.py
+++ nglpy_cuda/__init__.py
@@ -6,4 +6,7 @@ __author__ = """Daniel Patrick Maljovec"""
 __email__ = 'maljovec002@gmail.com'
 __version__ = '0.1.0'
 from .Graph import Graph
+from .SKLSearchIndex import SKLSearchIndex
+from .FAISSSearchIndex import FAISSSearchIndex
+from .utils import *
 from nglpy_cuda.core import *
