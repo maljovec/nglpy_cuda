076db215bb5fdfb9ee44db6d4d2f49bcdbbd6b18
Dan Maljovec <maljovec@cs.utah.edu>
2018-09-25 00:53:47 -0600

    fixup

--- include/ngl_cuda.cuh
+++ include/ngl_cuda.cuh
@@ -5,7 +5,7 @@
 namespace nglcu{
     void create_template(float * data, float beta=1, int p=2, int steps=100);
     float min_distance_from_edge(float t, float beta, float p);
-    void associate_probability(float *X, int *edges, int *indices,
+    void associate_probability(float *X, int *edges, float *probabilities, int *indices,
                                int N, int D, int M, int K, float steepness=3,
                                bool relaxed=false, float beta=1, float lp=2,
                                int count=-1);
